3e49c788e988fc18a37f723132e293a7e8c0a272
Code IT <codeit.np@gmail.com>
2023-06-01 14:19:58 +0545

    authentication added

--- student-management-system/app/Http/Controllers/Api/AuthController.php
+++ student-management-system/app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -14,6 +15,17 @@ class AuthController extends Controller
     public function register(Request $request)
     {
 
+        $rules = array(
+            'name' => 'required',
+            'password' => 'required',
+            'email' => 'required|unique:users,email',
+        );
+
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'status' => 403, 'data' => $validator->errors()], 403);
+        }
+
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
@@ -21,6 +33,31 @@ class AuthController extends Controller
         $user->company_id = $request->company_id;
         $user->save();
 
-        return response()->json(["success" => true, "message" => "User created successfully"]);
+        return response()->json(["success" => true, 'status' => 201, "message" => "User created successfully"], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $rules = array(
+            'email' => 'required|email',
+            'password' => 'required',
+        );
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'status' => 403, 'data' => $validator->errors()], 403);
+        }
+
+
+        $user = User::where('email', $request->email)->first();
+
+
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['success' => false, 'message' => 'Invalid username or password'], 403);
+        }
+
+        $token = $user->createToken($request->email)->plainTextToken;
+
+        return response()->json(['success' => true, 'status' => 200, 'token' => $token, 'user' => $user], 200);
     }
 }
--- student-management-system/routes/api.php
+++ student-management-system/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use PharIo\Manifest\AuthorCollection;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Sanctum
+
+// Global
+// Authentication Routes
+Route::post("register", [AuthController::class, 'register']);
+Route::post("login", [AuthController::class, 'login']);
