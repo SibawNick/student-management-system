22b0afd5157161b4a6a8f138fbce6e6fc51066e3
KirawaYo365 <kirawa.ya.files@gmail.com>
2023-06-06 14:40:25 +0545

    Fiscal year api created

--- student-management-system/routes/api.php
+++ student-management-system/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CompanyController;
+use App\Http\Controllers\Api\FiscalYearController;
 use App\Http\Controllers\Api\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -25,6 +26,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Sanctum
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('company', CompanyController::class);
+    Route::apiResource('fiscal_years', FiscalYearController::class);
 });
 
 
